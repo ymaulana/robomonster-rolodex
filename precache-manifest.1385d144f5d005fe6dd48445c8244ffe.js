efb01258efa5663b83e5fd26cd5c725f1e1747a2
ymaulana <ymaulana1609@gmail.com>
2020-08-03 17:39:56 +0700

    Updates

---
+++ precache-manifest.1385d144f5d005fe6dd48445c8244ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8a10a225dc4a37df88d123d30f8703",
+    "url": "/robomonster-rolodex/index.html"
+  },
+  {
+    "revision": "df3a997d492c481bb322",
+    "url": "/robomonster-rolodex/static/css/main.ace1dcb8.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/robomonster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robomonster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3a997d492c481bb322",
+    "url": "/robomonster-rolodex/static/js/main.bba7d9d3.chunk.js"
+  },
+  {
+    "revision": "124682223d2e3fa25916",
+    "url": "/robomonster-rolodex/static/js/runtime-main.c3822d5d.js"
+  }
+]);
